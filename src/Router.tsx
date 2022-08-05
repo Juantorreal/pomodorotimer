eef86b8cd2d41254c5836752560c9bbf602735bd
Juan Torreal <juantorreal@gmail.com>
2022-08-04 23:44:26 -0300

    form

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { History } from "./pages/History";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { useForm } from "react-hook-form";
 
 export function Router() {
     return (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,32 @@ import { Play } from "phosphor-react";
 import React from "react";
 import { Header } from "../../components/Header";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
-
+import { useForm } from "react-hook-form";
 export function Home() {
     return (
       <HomeContainer>
         <form>
           <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="De um nome ao seu projeto"/>
+          <TaskInput id="task" placeholder="De um nome ao seu projeto" list="task-suggestions"/>
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Projeto 3"/>
+            <option value="manna"/>
+
+
+
+          </datalist>
           <label>durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput type="number"
+           id="minutesAmount" 
+           placeholder="00" 
+           step={5}
+           min={5}
+           max={60}
+           />
           <span>minutos.</span>
           </FormContainer>
         
